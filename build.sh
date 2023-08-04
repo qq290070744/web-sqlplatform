9753ba81e0fefe3b47db2c778a4b3e124329d4d1
Wenhui.Jiang <290070744@qq.com>
2023-08-04 16:18:14 +0800

    .

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-tag=satge-20230804.1
+tag=satge-20230804.2
 docker build -t registry.cn-shanghai.aliyuncs.com/jwh/web_sql_platform:$tag .
 docker push registry.cn-shanghai.aliyuncs.com/jwh/web_sql_platform:$tag
 echo $tag
