e00c7e3033b61aa13e1c37e09cc7b3e595ca62c6
jiangwenhui <290070744@qq.com>
2023-08-17 14:31:47 +0800

    2023-08-17

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-tag=satge-20230817.1
-npm run build
+tag=satge-20230817.2
+npm run build:stage
 docker build -t registry.cn-shanghai.aliyuncs.com/jwh/web_sql_platform:$tag .
 docker push registry.cn-shanghai.aliyuncs.com/jwh/web_sql_platform:$tag
 echo $tag
