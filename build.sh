febba39941b3269715fcc5a99bd7f2e2a8c5b04e
jiangwenhui <290070744@qq.com>
2023-08-28 14:50:59 +0800

    .

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 env=stage
 tag=${env}-20230823
-#npm run build:${env}
+npm run build:${env}
 docker build -t registry.cn-shanghai.aliyuncs.com/jwh/web_sql_platform:$tag .
 docker push registry.cn-shanghai.aliyuncs.com/jwh/web_sql_platform:$tag
 echo $tag
